c81f3e182acf4e22f33a1872ded52ffebfa0d96a
liyiming-serif <liyiming.serif@gmail.com>
2015-11-17 12:52:36 -0500

    Update implementation.c

--- houses/src/implementation.c
+++ houses/src/implementation.c
@@ -1,6 +1,17 @@
 #include "cacheofkings.h"
 
-/*Defeats NoOp Bot by writing to own mem*/
+/*Defeat greedy bot by writing to own mem more efficiently*/
 void __start(int core_id, int num_crashes, unsigned char payload) {
 	
+	int *ptr = (int *)HOME_DATA_SEGMENT + (rand() % HOME_DATA_SIZE);
+	
+  if (core_id >= 2) {
+    ptr += HIMEM;  // move pointer to opponent's half of memory
+    Sneak_Attack();
+  }
+
+  int i = 0;
+  while (1) {
+    ptr[i++] = payload;
+  }
 }
