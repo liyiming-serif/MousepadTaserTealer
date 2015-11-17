377d32d1ea94a277fdd411aa048c6eee0134d44f
liyiming-serif <liyiming.serif@gmail.com>
2015-11-17 15:31:25 -0500

    Update implementation.c

--- houses/src/implementation.c
+++ houses/src/implementation.c
@@ -2,8 +2,8 @@
 
 /*Defeat greedy bot by writing to own mem more efficiently*/
 void __start(int core_id, int num_crashes, unsigned char payload) {
-	
-	int *ptr = (int *)HOME_DATA_SEGMENT + (rand() % HOME_DATA_SIZE);
+  int *ptr = (int *)HOME_DATA_SEGMENT + (rand() % HOME_DATA_SIZE);
+  unsigned int payword; (unsigned int)(payload | payload << 8 | payload << 16 | payload << 24);
 	
   if (core_id >= 2) {
     ptr += HIMEM;  // move pointer to opponent's half of memory
@@ -12,6 +12,6 @@ void __start(int core_id, int num_crashes, unsigned char payload) {
 
   int i = 0;
   while (1) {
-    ptr[i++] = payload;
+    ptr[i++] = payword;
   }
 }
