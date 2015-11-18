11442c2f081579037ecec56518ac57cfc6dbdb7f
liyiming-serif <liyiming.serif@gmail.com>
2015-11-17 21:42:15 -0500

    Update implementation.c

--- houses/src/implementation.c
+++ houses/src/implementation.c
@@ -3,7 +3,9 @@
 /*Defeat greedy bot by writing to own mem more efficiently*/
 void __start(int core_id, int num_crashes, unsigned char payload) {
   int *ptr = (int *)HOME_DATA_SEGMENT + (rand() % HOME_DATA_SIZE);
-  unsigned int payword; (unsigned int)(payload | payload << 8 | payload << 16 | payload << 24);
+  //write 4 copies of the payload into four bytes at once.
+  unsigned int p = (unsigned int) payload;
+  unsigned int payword = p | p << 8 | p << 16 | p << 24;
 	
   if (core_id >= 2) {
     ptr += HIMEM;  // move pointer to opponent's half of memory
