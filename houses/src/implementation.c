c5a823e431de3ff454ebb1b86caf6167e8e030bd
liyiming-serif <liyiming.serif@gmail.com>
2015-11-18 01:17:35 -0500

    Update implementation.c

--- houses/src/implementation.c
+++ houses/src/implementation.c
@@ -13,14 +13,19 @@ void __start(int core_id, int num_crashes, unsigned char payload)
     
   }
   
-  if(core_id == 1) //works on cacheline w. index = 01
+  else if(core_id == 1) //works on cacheline w. index = 01
   {
-  	ptr += 64; //64 words per cacheline
+  	ptr += CACHE_LINE/4; //advance by 1 cacheline
   }
 	
-  if (core_id >= 2) {
+  else if (core_id == 2) {
     ptr += HIMEM;  // move pointer to opponent's half of memory
     Sneak_Attack();
+  }
+  
+  else if(core_id == 3) {
+  	ptr += HIMEM + 64;
+  	Sneak_Attack();
   }
 
   int i = 0;
