52d0f0a61659b81f06a39fdd538c10012cd9aa36
liyiming-serif <liyiming.serif@gmail.com>
2015-11-18 02:17:35 -0500

    Update implementation.c

--- houses/src/implementation.c
+++ houses/src/implementation.c
@@ -10,26 +10,31 @@ void __start(int core_id, int num_crashes, unsigned char payload)
   
   if(core_id == 0) //works on cacheline w. index = 00
   {
-    
   }
   
   else if(core_id == 1) //works on cacheline w. index = 01
   {
-  	ptr += CACHE_LINE/4; //advance by 1 cacheline
+    ptr += CACHE_LINE/4; //advance by 1 cacheline
   }
 	
-  else if (core_id == 2) {
-    ptr += (int*)HIMEM;  // move pointer to opponent's half of memory
+  else if (core_id == 2) { //works on cacheline 10 (opponent)
+    ptr += (int*)HIMEM;
     Sneak_Attack();
   }
   
-  else if(core_id == 3) {
+  else if(core_id == 3) { //workds on cacheline 11 (opponent)
   	ptr += (int*)HIMEM + CACHE_LINE/4;
   	Sneak_Attack();
   }
 
-  int i = 0;
-  while (1) {
+  int i = 0; //offset
+  //main loop
+  while (1) { //TODO: detect when to prefetch here
+    if(i > (CACHE_LINE/4))
+    {
+      i = 0; //reset offset when end of block is reached
+      ptr += 2*CACHE_LINE/4; //next blocks of mem for an index are 2 cachelines down
+    }
     ptr[i++] = payword;
   }
 }
